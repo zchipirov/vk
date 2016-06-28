fbb162964c3e9293be768437e17af2fd3c60446a
chipirov <chipirov@yandex.ru>
2016-06-28 16:59:42 +0300

    new auth method 12

--- vk/data.php
+++ vk/data.php
@@ -21,9 +21,9 @@ if (isset($_POST['action']) && $_POST['action'] == 'search') {
 	ini_set('max_execution_time', 3600000);
 	if (isset($_POST['audio'])) {
 			var_dump($_POST['data']);
-			return;
+			return json_encode(array('a'));
 	}
-	return json_decode("");
+	return json_encode(array('b'));
 	for ($i = 0; $i < count($data['response']['items']); $i++) {
 		$userid = $data['response']['items'][$i];
 		
