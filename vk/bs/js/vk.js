a6ed9ebf73540b17556574f6c2d36e13c7aba687
chipirov <chipirov@yandex.ru>
2016-06-28 12:47:47 +0300

    new auth method 9

--- vk/bs/js/vk.js
+++ vk/bs/js/vk.js
@@ -4,50 +4,53 @@ VK.init({
 
 VK.Auth.login(function(response) {
   if (response.session) {
-    /* Пользователь успешно авторизовался */
+    /* РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ СѓСЃРїРµС€РЅРѕ Р°РІС‚РѕСЂРёР·РѕРІР°Р»СЃСЏ */
     if (response.settings) {
-      /* Выбранные настройки доступа пользователя, если они были запрошены */
+      /* Р’С‹Р±СЂР°РЅРЅС‹Рµ РЅР°СЃС‚СЂРѕР№РєРё РґРѕСЃС‚СѓРїР° РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ, РµСЃР»Рё РѕРЅРё Р±С‹Р»Рё Р·Р°РїСЂРѕС€РµРЅС‹ */
     }
   } else {
-    /* Пользователь нажал кнопку Отмена в окне авторизации */
+    /* РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ РЅР°Р¶Р°Р» РєРЅРѕРїРєСѓ РћС‚РјРµРЅР° РІ РѕРєРЅРµ Р°РІС‚РѕСЂРёР·Р°С†РёРё */
   }
 });
 
-var membersGroups = []; // массив участников группы
+var membersGroups = []; // РјР°СЃСЃРёРІ СѓС‡Р°СЃС‚РЅРёРєРѕРІ РіСЂСѓРїРїС‹
 getMembers('ansarsharia');
 
-// получаем информацию о группе и её участников
+// РїРѕР»СѓС‡Р°РµРј РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ РіСЂСѓРїРїРµ Рё РµС‘ СѓС‡Р°СЃС‚РЅРёРєРѕРІ
 function getMembers(gr_id) {
-	alert(gr_id);
+	
 	VK.Api.call('groups.getById', {group_id: gr_id, fields: 'photo_50,members_count', v: '5.52'}, function(r) {
 			if(r.response) {
 				alert(r.response[0].members_count);
-				getMembers20k(gr_id, r.response[0].members_count); // получаем участников группы и пишем в массив membersGroups
+				getMembers20k(gr_id, r.response[0].members_count); // РїРѕР»СѓС‡Р°РµРј СѓС‡Р°СЃС‚РЅРёРєРѕРІ РіСЂСѓРїРїС‹ Рё РїРёС€РµРј РІ РјР°СЃСЃРёРІ membersGroups
+				membersGroups.forEach(function(item, i, membersGroups) {
+				  alert( i + ": " + item + " (РјР°СЃСЃРёРІ:" + membersGroups + ")" );
+				});
 			}
 	});
 }
 
-// получаем участников группы, members_count - количество участников
+// РїРѕР»СѓС‡Р°РµРј СѓС‡Р°СЃС‚РЅРёРєРѕРІ РіСЂСѓРїРїС‹, members_count - РєРѕР»РёС‡РµСЃС‚РІРѕ СѓС‡Р°СЃС‚РЅРёРєРѕРІ
 function getMembers20k(group_id, members_count) {
-	var code =  'var members = API.groups.getMembers({"group_id": ' + "\""+group_id + "\"" + ', "v": "5.52", "sort": "id_asc", "count": "1000", "offset": ' + membersGroups.length + '}).items;' // делаем первый запрос и создаем массив
-			+	'var offset = 1000;' // это сдвиг по участникам группы
-			+	'while (offset < 25000 && (offset + ' + membersGroups.length + ') < ' + members_count + ')' // пока не получили 20000 и не прошлись по всем участникам
+	var code =  'var members = API.groups.getMembers({"group_id": ' + "\""+group_id + "\"" + ', "v": "5.52", "sort": "id_asc", "count": "1000", "offset": ' + membersGroups.length + '}).items;' // РґРµР»Р°РµРј РїРµСЂРІС‹Р№ Р·Р°РїСЂРѕСЃ Рё СЃРѕР·РґР°РµРј РјР°СЃСЃРёРІ
+			+	'var offset = 1000;' // СЌС‚Рѕ СЃРґРІРёРі РїРѕ СѓС‡Р°СЃС‚РЅРёРєР°Рј РіСЂСѓРїРїС‹
+			+	'while (offset < 25000 && (offset + ' + membersGroups.length + ') < ' + members_count + ')' // РїРѕРєР° РЅРµ РїРѕР»СѓС‡РёР»Рё 20000 Рё РЅРµ РїСЂРѕС€Р»РёСЃСЊ РїРѕ РІСЃРµРј СѓС‡Р°СЃС‚РЅРёРєР°Рј
 			+	'{'
-				+	'members = members + "," + API.groups.getMembers({"group_id": ' + "\""+group_id + "\"" + ', "v": "5.52", "sort": "id_asc", "count": "1000", "offset": (' + membersGroups.length + ' + offset)}).items;' // сдвиг участников на offset + мощность массива
-				+	'offset = offset + 1000;' // увеличиваем сдвиг на 1000
+				+	'members = members + "," + API.groups.getMembers({"group_id": ' + "\""+group_id + "\"" + ', "v": "5.52", "sort": "id_asc", "count": "1000", "offset": (' + membersGroups.length + ' + offset)}).items;' // СЃРґРІРёРі СѓС‡Р°СЃС‚РЅРёРєРѕРІ РЅР° offset + РјРѕС‰РЅРѕСЃС‚СЊ РјР°СЃСЃРёРІР°
+				+	'offset = offset + 1000;' // СѓРІРµР»РёС‡РёРІР°РµРј СЃРґРІРёРі РЅР° 1000
 			+	'};'
-			+	'return members;'; // вернуть массив members
-	alert(code);
+			+	'return members;'; // РІРµСЂРЅСѓС‚СЊ РјР°СЃСЃРёРІ members
+	
 	VK.Api.call("execute", {code: code}, function(data) {
 		if (data.response) {
-			membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]")); // запишем это в массив
-			$('.member_ids').html('Загрузка: ' + membersGroups.length + '/' + members_count);
-			if (members_count >  membersGroups.length) // если еще не всех участников получили
-				setTimeout(function() { getMembers20k(group_id, members_count); }, 333); // задержка 0.333 с. после чего запустим еще раз
-			else // если конец то
-				alert('Ура тест закончен! В массиве membersGroups теперь ' + membersGroups.length + ' элементов.');
+			membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]")); // Р·Р°РїРёС€РµРј СЌС‚Рѕ РІ РјР°СЃСЃРёРІ
+			$('.member_ids').html('Р—Р°РіСЂСѓР·РєР°: ' + membersGroups.length + '/' + members_count);
+			if (members_count >  membersGroups.length) // РµСЃР»Рё РµС‰Рµ РЅРµ РІСЃРµС… СѓС‡Р°СЃС‚РЅРёРєРѕРІ РїРѕР»СѓС‡РёР»Рё
+				setTimeout(function() { getMembers20k(group_id, members_count); }, 333); // Р·Р°РґРµСЂР¶РєР° 0.333 СЃ. РїРѕСЃР»Рµ С‡РµРіРѕ Р·Р°РїСѓСЃС‚РёРј РµС‰Рµ СЂР°Р·
+			else // РµСЃР»Рё РєРѕРЅРµС† С‚Рѕ
+				alert('РЈСЂР° С‚РµСЃС‚ Р·Р°РєРѕРЅС‡РµРЅ! Р’ РјР°СЃСЃРёРІРµ membersGroups С‚РµРїРµСЂСЊ ' + membersGroups.length + ' СЌР»РµРјРµРЅС‚РѕРІ.');
 		} else {
-			alert(data.error.error_msg); // в случае ошибки выведем её
+			alert(data.error.error_msg); // РІ СЃР»СѓС‡Р°Рµ РѕС€РёР±РєРё РІС‹РІРµРґРµРј РµС‘
 		}
 	});
 }
