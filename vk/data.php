71a0fe93155d249fedc527a6a5768a958eed3c01
chipirov <chipirov@yandex.ru>
2016-06-28 17:00:44 +0300

    new auth method 12

--- vk/data.php
+++ vk/data.php
@@ -24,7 +24,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'search') {
 			return json_encode(array('a'));
 	}
 	return json_encode(array('b'));
-	for ($i = 0; $i < count($data['response']['items']); $i++) {
+	/*for ($i = 0; $i < count($data['response']['items']); $i++) {
 		$userid = $data['response']['items'][$i];
 		
 		if (isset($_POST['audio'])) {
@@ -43,33 +43,5 @@ if (isset($_POST['action']) && $_POST['action'] == 'search') {
 				}
 			}
 		}
-	}
-	
-	ini_set('max_execution_time', 30);
-	if (isset($audio_) || isset($video_) || isset($docs_) || isset($groups_) || isset($friends_)) {
-		if (isset($audio_)) $smarty->assign("audio_", $audio_);
-		if (isset($video_)) $smarty->assign("video_", $video_);
-		if (isset($docs_)) $smarty->assign("docs_", $docs_);
-		if (isset($groups_)) $smarty->assign("groups_", $groups_);
-		if (isset($friends_)) $smarty->assign("friends_", $friends_);
-		
-		$smarty->display('result.tpl');
-		return;
-	}
-		
+	}*/
 }
-
-$user->Check();
-//$access_token = $user->GetAccess_token();
-
-$auth = new AuthController();
-
-$list = $list->Select();
-$smarty->assign("lists", $list);
-
-$groups = $group->Select();
-$smarty->assign("groups", $groups);
-$smarty->assign("home", 1);
-
-//if ($auth->VK_Auth($access_token))
-	$smarty->display('index.tpl');
