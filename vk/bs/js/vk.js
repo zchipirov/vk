e3d5715cbbcf6a94bf11293ab4cb114ae13cb0d4
chipirov <chipirov@yandex.ru>
2016-06-28 16:11:59 +0300

    new auth method 12

--- vk/bs/js/vk.js
+++ vk/bs/js/vk.js
@@ -60,8 +60,8 @@ function getMembers20k(group_id, members_count) {
 					alert(code2);
 					VK.Api.call("execute", {code: code2}, function(data) {
 						if (data.response) {
-							audio = audio.concat(JSON.parse("[" + data.response + "]"));
-							console.log(audio);
+							//audio = audio.concat(JSON.parse("[" + data.response + "]"));
+							console.log(data.response);
 						} else {
 							alert(data.error.error_msg); // в случае ошибки выведем её
 						}
