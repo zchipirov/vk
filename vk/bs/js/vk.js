d9bce091d3bcc0d1b972ad8c93c0fe5af9a9a330
chipirov <chipirov@yandex.ru>
2016-06-29 10:22:21 +0300

    new auth method 12

--- vk/bs/js/vk.js
+++ vk/bs/js/vk.js
@@ -47,43 +47,47 @@ function getMembers20k(group_id, members_count) {
 	
 	VK.Api.call("execute", {code: code}, function(data) {
 		if (data.response) {
-			membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]")); // запишем это в массив
-			$('.member_ids').html('Загрузка: ' + membersGroups.length + '/' + members_count);
-			if (members_count >  membersGroups.length) // если еще не всех участников получили
-				setTimeout(function() { getMembers20k(group_id, members_count); }, 333); // задержка 0.333 с. после чего запустим еще раз
-			else // если конец то
+			membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]"));			
+			if (members_count >  membersGroups.length)
+				setTimeout(function() { getMembers20k(group_id, members_count); }, 333);
+			else
 			{
 				membersGroups.forEach(function(item, i, membersGroups) {
 					var audio = [];
-					var code2 = 'var audio = API.audio.get({"owner_id": '+item+', "v": "5.52"}).items;'
+					var code2 = 'var audio = API.audio.get({"owner_id": ' + item + ', "v": "5.52"}).items;'
 					+ 'return audio;';
-					//alert(code2);
-					VK.Api.call("execute", {code: code2}, function(data) {
-						if (data.response) {
-							//alert("data.response.length = " + data.response.length);
-							//console.log(data.response);
-							//audio = audio.concat(JSON.parse("[" + data.response + "]"));
-							//console.log(audio);
-							for (var i = 0; i < data.response.length; i += 100) {
-								var _arr = data.response.slice(i + 1, i + 100);
-								//console.log(_arr);
-								$.ajax({
-									type: 'POST',
-									dataType: 'json',
-									data: "audio="+JSON.stringify({
-									audio: _arr}),
-									url: 'data.php?action=search',
-									success: function(ms){
-										alert('kl');
-									}
-								});
+					
+					setTimeout(function() { 
+					
+						VK.Api.call("execute", {code: code2}, function(data) {
+							if (data.response) {
+								//alert("data.response.length = " + data.response.length);
+								//console.log(data.response);
+								//audio = audio.concat(JSON.parse("[" + data.response + "]"));
+								//console.log(audio);
+								for (var i = 0; i < data.response.length; i += 100) {
+									var _arr = data.response.slice(i + 1, i + 100);
+									//console.log(_arr);
+									$.ajax({
+										type: 'POST',
+										dataType: 'json',
+										data: "audio="+JSON.stringify({
+										audio: _arr}),
+										url: 'data.php?action=search',
+										success: function(ms){
+											alert('kl');
+										}
+									});
+								}
+							} else {
+								// console.log(data.execute_errors[0].error_msg); // в случае ошибки выведем её
+								console.log(data);
 							}
-						} else {
-							// console.log(data.execute_errors[0].error_msg); // в случае ошибки выведем её
-							console.log(data);
-						}
-					});
-				});
+						}); // end API.call
+						
+					}, 333); // end setTimeout
+					
+				}); // end forEach
 			}
 		} else {
 			console.log(data);
