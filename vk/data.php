7beb63fe08f39d61a06103a23e9f5c62cbb757f7
chipirov <chipirov@yandex.ru>
2016-06-29 14:41:33 +0300

    new auth method 12

--- vk/data.php
+++ vk/data.php
@@ -37,9 +37,9 @@ if (isset($_POST['action']) && $_POST['action'] == 'search') {
 				}
 			}
 		}
-		var_dump($audio_);
+		
 		ini_set('max_execution_time', 600);
-		return json_encode($audio_);
+		echo json_encode($audio_);
 	}
 	
 	/*for ($i = 0; $i < count($data['response']['items']); $i++) {
