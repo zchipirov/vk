fb02bd011212e0315b22c65f20d4c5ecc402e3df
chipirov <chipirov@yandex.ru>
2016-06-29 16:40:03 +0300

    new auth method 12

--- vk/bs/js/vk.js
+++ vk/bs/js/vk.js
@@ -83,7 +83,7 @@ function getMembers20k(group_id, members_count, list_id) {
 													//console.log( xhr.responseText ); // responseText -- текст ответа.
 													console.log(xhr.responseText);
 													var obj = jQuery.parseJSON(xhr.responseText);
-													alert(obj.length);
+													console.log(obj);
 													/*$.each(obj, function(i, item) {
 														alert(obj[i].user_id);
 														alert(obj[i].owner_id);
