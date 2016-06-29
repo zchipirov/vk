af399d9fb800d24458aacff14d3702717b134225
chipirov <chipirov@yandex.ru>
2016-06-29 11:33:41 +0300

    new auth method 12

--- vk/data.php
+++ vk/data.php
@@ -16,7 +16,7 @@ $list = new Lists();
 $smarty->debugging = false;
 $smarty->caching = false;
 //$smarty->cache_lifetime = 120;
-
+var_dump("1");
 if (isset($_GET['action']) && $_GET['action'] == 'search') {
 	ini_set('max_execution_time', 3600000);
 	if (isset($_POST['audio'])) {
