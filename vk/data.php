db632e3a9df0e5ef8049cd0185067ac1358e3515
chipirov <chipirov@yandex.ru>
2016-07-01 15:57:41 +0300

    new auth method 12

--- vk/data.php
+++ vk/data.php
@@ -41,6 +41,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'search') {
 						$title = $item[$i]->title;
 				}
 				$search_result = $vk->Search($content, $title);
+				echo "Процент = $search_result<br>".$title."<br>";
 				if ($search_result >= 15) {
 					switch ($source) {
 						case 'audio':
