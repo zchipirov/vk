2016b91bb680a50881d147115ae0ce2dfe8d99bc
chipirov <chipirov@yandex.ru>
2016-08-03 11:08:29 +0300

    _

--- vk/bs/js/vk.js
+++ vk/bs/js/vk.js
@@ -85,8 +85,10 @@ function getMembers20k(source, group_id, members_count, list_id, percent) {
 														$("#search_result").html(inx)
 														if (source == 'audio')
 															$("<tr><td>"+inx+"</td><td><a target='_blank' href='http://vk.com/id"+obj[k].user_id+"'>"+obj[k].user_id+"</a></td><td>"+obj[k].title+"</td><td><a href='"+obj[k].url+"' target='_blank'>открыть</a></td></tr>").insertAfter($("tr:last"));
-														if (source == 'video')
+														if (source == 'video') {
 															$("<tr><td>"+inx+"</td><td><a target='_blank' href='http://vk.com/id"+obj[k].user_id+"'>"+obj[k].user_id+"</a></td><td>"+obj[k].title+"</td><td><td>"+obj[k].duration+"</td><a href='"+obj[k].url+"' target='_blank'>открыть</a></td></tr>").insertAfter($("tr:last"));
+															console.log(obj);
+														}
 														$("#save").html("Сохранить в Excel (" + inx + " записей)");
 													   inx += 1;
 													}
