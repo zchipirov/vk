a5ff8a5b46107b1c6cec6c3ce8665004918aad67
chipirov <chipirov@yandex.ru>
2016-08-10 10:57:56 +0300

    1

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <html>
 <head>
-<title>Перенаправление на служебный сайт Денвера...</title>
-<script src="//vk.com/js/api/xd_connection.js?2"  type="text/javascript"></script>
-<script type="text/javascript"> 
-	VK.init(function() { 
-		alert('succeeded');
-	}, function() {
-		alert('failed');
-	}, '5.50'); 
-	function UsersGet() {
-		alert('a');
-		VK.api("users.get", {user_ids:"299472499"}, function(data) { 
-			alert('a');
-		});
-	}
-	UsersGet();
-</script>
+<title>Перенаправление на служебный сайт...</title>
+<?php
+	header('Location: http://vkgrab.esy.es/vk/index.php');
+	exit;
+?>
 </head>
 </html>
