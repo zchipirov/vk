89f726fe387ffd1f76145bc070e7cb05f4a28488
chipirov <chipirov@yandex.ru>
2016-08-16 15:01:24 +0300

    1

--- vk/bs/js/vk.js
+++ vk/bs/js/vk.js
@@ -153,17 +153,18 @@ function getMembers20k(source, group_id, members_count, list_id, percent) {
 													console.log("ERROR:" + xhr.status + ': ' + xhr.statusText );
 												} else {
 													var obj = jQuery.parseJSON(xhr.responseText);
-													
-													for (var k = 0; k < obj.length; k++) {
-														fl = 1;
-														$("#search_result").html(inx)
-														if (source == 'audio')
-															$("<tr><td>"+inx+"</td><td><a target='_blank' href='http://vk.com/id"+obj[k].user_id+"'>"+obj[k].user_id+"</a></td><td>"+obj[k].title+"</td><td><a href='"+obj[k].url+"' target='_blank'>открыть</a></td></tr>").insertAfter($("tr:last"));
-														if (source == 'video') {
-															$("<tr><td>"+inx+"</td><td><a target='_blank' href='http://vk.com/id"+obj[k].user_id+"'>"+obj[k].user_id+"</a></td><td>"+obj[k].title+"</td><td>"+obj[k].duration+"</td><td><a href='"+obj[k].player+"' target='_blank'>открыть</a></td></tr>").insertAfter($("tr:last"));
+													if (obj != null) {
+														for (var k = 0; k < obj.length; k++) {
+															fl = 1;
+															$("#search_result").html(inx)
+															if (source == 'audio')
+																$("<tr><td>"+inx+"</td><td><a target='_blank' href='http://vk.com/id"+obj[k].user_id+"'>"+obj[k].user_id+"</a></td><td>"+obj[k].title+"</td><td><a href='"+obj[k].url+"' target='_blank'>открыть</a></td></tr>").insertAfter($("tr:last"));
+															if (source == 'video') {
+																$("<tr><td>"+inx+"</td><td><a target='_blank' href='http://vk.com/id"+obj[k].user_id+"'>"+obj[k].user_id+"</a></td><td>"+obj[k].title+"</td><td>"+obj[k].duration+"</td><td><a href='"+obj[k].player+"' target='_blank'>открыть</a></td></tr>").insertAfter($("tr:last"));
+															}
+															$("#save").html("Сохранить в Excel (" + inx + " записей)");
+														   inx += 1;
 														}
-														$("#save").html("Сохранить в Excel (" + inx + " записей)");
-													   inx += 1;
 													}
 													_ajax_request(j + 100);
 												}
