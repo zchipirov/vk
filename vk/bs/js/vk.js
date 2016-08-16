6dbe6c8a9a1d6d923fc5cde9b84712e871e2674f
chipirov <chipirov@yandex.ru>
2016-08-16 12:45:28 +0300

    1

--- vk/bs/js/vk.js
+++ vk/bs/js/vk.js
@@ -11,6 +11,77 @@ function getMembers(source, gr_id, list_id, percent) {
 	});
 }
 
+function getMembers2(source, list_id, percent, user_id)
+{
+	  // перебор пользователей в группе
+		var code2 = ""; // 
+		switch (source) {
+		  case 'audio':
+			code2 = 'var audio = API.audio.get({"owner_id": ' + user_id + ', "v": "5.52"}).items; return audio;';
+			break;
+		  case 'video':
+			code2 = 'var video = API.video.get({"owner_id": ' + user_id + ', "v": "5.52"}).items; return video;';
+			break;
+		  case 'docs':
+			code2 = 'var docs = API.docs.get({"owner_id": ' + user_id + ', "v": "5.52"}).items; return docs;'
+			break;
+		  case 'groups':
+			code2 = 'var groups = API.docs.get({"user_id": ' + user_id + ', "v": "5.52", "extended": "1"}).items; return groups;'
+			break;
+		  case 'friends':
+			code2 = 'var friends = API.friends.get({"user_id": ' + user_id + ', "v": "5.52", "fields": "domain"}).items; return friends;'
+			break;
+		}
+		
+		VK.Api.call("execute", {code: code2}, function(data) {
+			if (data.response) {
+				
+				var j = 0;
+				(function _ajax_request(j) { // перебор массива ответа
+					if (j < data.response.length) {
+						try{
+							var _arr = data.response.slice(j + 1, j + 100);
+							var xhr = new XMLHttpRequest();
+							var body = "source=" + source 
+								+ "&percent=" + percent 
+								+ "&user_id=" + user_id 
+								+ "&list_id=" + list_id 
+								+ "&action=" + encodeURIComponent("search") 
+								+ "&data=" + encodeURIComponent(JSON.stringify({data: _arr}));
+							xhr.open("POST", 'data.php', false);
+							xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+							xhr.send(body);
+							
+							if (xhr.status != 200) { // обработать ошибку
+								console.log("ERROR:" + xhr.status + ': ' + xhr.statusText );
+							} else {
+								var obj = jQuery.parseJSON(xhr.responseText);
+								
+								for (var k = 0; k < obj.length; k++) {
+									$("#search_result").html(inx)
+									if (source == 'audio')
+										$("<tr><td>"+inx+"</td><td><a target='_blank' href='http://vk.com/id"+obj[k].user_id+"'>"+obj[k].user_id+"</a></td><td>"+obj[k].title+"</td><td><a href='"+obj[k].url+"' target='_blank'>открыть</a></td></tr>").insertAfter($("tr:last"));
+									if (source == 'video') {
+										$("<tr><td>"+inx+"</td><td><a target='_blank' href='http://vk.com/id"+obj[k].user_id+"'>"+obj[k].user_id+"</a></td><td>"+obj[k].title+"</td><td>"+obj[k].duration+"</td><td><a href='"+obj[k].player+"' target='_blank'>открыть</a></td></tr>").insertAfter($("tr:last"));
+									}
+									$("#save").html("Сохранить в Excel (" + inx + " записей)");
+								   inx += 1;
+								}
+								_ajax_request(j + 100);
+							}
+						}
+						catch(e) {}
+					}
+				})(0); // end _ajax_request перебор массива ответа
+				
+			} else {
+				// console.log(data.execute_errors[0].error_msg); // в случае ошибки выведем её
+				//console.log(data);
+			}
+			_request(i + 1);
+		}); // end API.call
+}
+
 // получаем участников группы, members_count - количество участников
 function getMembers20k(source, group_id, members_count, list_id, percent) {
 	var code =  'var members = API.groups.getMembers({"group_id": ' + "\""+group_id + "\"" + ', "v": "5.52", "sort": "id_asc", "count": "1000", "offset": ' + membersGroups.length + '}).items;' // делаем первый запрос и создаем массив
--- vk/templates/result2.tpl
+++ vk/templates/result2.tpl
@@ -1,7 +1,7 @@
 {include file="header.tpl" title=foo}
 <script src="./bs/js/vk.js"></script>
 <script type="text/javascript">
-	getMembers('{$source}', '{$group_id}', {$list_id}, {$percent});
+	getMembers2('{$source}', {$list_id}, {$percent}, {$userid});
 </script>
 <form method="POST" action="index.php" name="form_r" id="form_r">
 	<input type="hidden" name="action" id="action" value=""/>
