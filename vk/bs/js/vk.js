eb11bb2ff6c860080be80e0631f49f05dc3c7975
chipirov <chipirov@yandex.ru>
2016-08-22 14:29:23 +0300

    1

--- vk/bs/js/vk.js
+++ vk/bs/js/vk.js
@@ -50,7 +50,7 @@ function getMembers2(source, list_id, percent, user_id)
 							+ "&list_id=" + list_id 
 							+ "&action=" + encodeURIComponent("search") 
 							+ "&data=" + encodeURIComponent(JSON.stringify({data: _arr}));
-						xhr.open("POST", 'data.php', true);
+						xhr.open("POST", 'data.php', false);
 						xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 						xhr.send(body);
 						
