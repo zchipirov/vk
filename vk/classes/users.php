baef403a678a875a970292f7d78dbc63e2bd1d0a
chipirov <chipirov@yandex.ru>
2016-08-24 11:56:41 +0300

    1

--- vk/classes/users.php
+++ vk/classes/users.php
@@ -101,7 +101,7 @@ class Users {
 	}
 	public function UpdateRSS($title, $link, $description, $pubDate) {
 		$db = new SafeMySQL();
-		$sql = "INSERT INTO rss (title, link, desc, dt) VALUE ('".$title."', '".$link."', '".$description."', '".$pubDate."')";
+		$sql = "INSERT INTO rss (title, link, desc, dt) VALUE ('".$title."', '".$link."', '".mysql_real_escape_string($description)."', '".$pubDate."')";
 		echo $sql;
 		$db->query($sql);
 	}
