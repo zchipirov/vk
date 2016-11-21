f552885785144a5ca6d61efe4c986d2589e0b4fd
chipirov <chipirov@yandex.ru>
2016-11-21 12:21:52 +0300

    1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <title>Перенаправление на служебный сайт...</title>
 <?php
-	header('Location: http://vkgrab.esy.es/vk/index.php');
+	header('Location: http://vkgrab.xyz/vk/index.php');
 	exit;
 ?>
 </head>
